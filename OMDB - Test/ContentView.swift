47085d5c4da2a8895cbcb311ab8cb343e4b0eae7
Preston Grisham <preston@prestongrisham.com>
2020-06-13 09:29:57 -0400

    Initial Commit

---
+++ OMDB - Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OMDB - Test
+//
+//  Created by Preston Grisham on 6/13/20.
+//  Copyright © 2020 Preston Grisham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
